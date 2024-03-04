62e5ae508a30069e27eb29e03d74ed2ca7385fd9
ehausafus <esh00002@mix.wvu.edu>
2024-03-03 22:52:28 -0500

    card and navbar inserted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Navbar.js';
+import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Card />
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
       <p>WVU ID: 800354906</p>
