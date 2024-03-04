e5ee773ff3c99ad941205542c0dcded605eef9ff
ehausafus <esh00002@mix.wvu.edu>
2024-03-04 13:59:20 -0500

    test

--- src/Card.js
+++ src/Card.js
@@ -1,33 +1,15 @@
 import React from 'react';
 function Card() {
     return (
-      <div className="row">
-      <div className="col">
-        <div className="card text-white bg-primary mb-3" style={{ maxWidth: '18rem' }}>
-          <div className="card-header text-center"><b>Card 01</b></div>
-          <div className="card-body text-center"> {/* Added text-center class here */}
-            <p className="card-text" style={{ fontSize: '0.9rem' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card text-white bg-danger mb-3" style={{ maxWidth: '18rem' }}>
-          <div className="card-header text-center"><b>Card 02</b></div>
-          <div className="card-body text-center"> {/* Added text-center class here */}
-            <p className="card-text" style={{ fontSize: '0.9rem' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card text-white bg-success mb-3" style={{ maxWidth: '18rem' }}>
-          <div className="card-header text-center"><b>Card 03</b></div>
-          <div className="card-body text-center"> {/* Added text-center class here */}
-            <p className="card-text" style={{ fontSize: '0.9rem' }}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
+      <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
       </div>
     </div>
-
     );
 }
 export default Card;
